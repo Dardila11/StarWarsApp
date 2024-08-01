11d0e242397ba660c0ab0629ef7eb6a8820d4045
Daniel Ardila <danieleardila11@gmail.com>
2024-08-01 10:36:05 -0400

    add character physical attributes

--- src/app/people/[id]/page.tsx
+++ src/app/people/[id]/page.tsx
@@ -1,4 +1,6 @@
+import H1 from "@/components/H1"
 import { Character } from "@/lib/types"
+import Image from "next/image"
 
 type CharacterDetailsPageProps = {
   params: {
@@ -12,5 +14,35 @@ export default async function CharacterDetailsPage({
   const response = await fetch(`https://swapi.dev/api/people/${id}`)
   const character: Character = await response.json()
 
-  return <div>{character.name}</div>
+  return (
+    <main className="flex flex-col items-center pt-5">
+      <H1>{character.name}</H1>
+      <section className="grid grid-cols-2 gap-4 mt-4">
+        <Image
+          className="rounded-e text-center"
+          src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
+          width={200}
+          height={200}
+          alt={character.name}
+        />
+
+        <section>
+          <p>Gender: {character.gender}</p>
+          <p>Hair Color: {character.hair_color}</p>
+          <p>Eye Color: {character.eye_color}</p>
+          <p>Height: {character.height}</p>
+          <p>Mass: {character.mass}</p>
+          <p>Birth Year: {character.birth_year}</p>
+        </section>
+
+        {/* <div>
+          <p>Homeworld: {character.homeworld}</p>
+          <p>Species: {character.species}</p>
+          <p>Starships: {character.starships}</p>
+          <p>Vehicles: {character.vehicles}</p>
+          <p>Films: {character.films}</p>
+        </div> */}
+      </section>
+    </main>
+  )
 }
